5f9ad4e54eb0c18f62fbd784cb6b332ac8a0f4e0
Michel S <michel.saglier@hove.com>
2023-12-11 17:45:14 +0100

    channel - nominal case

--- TR API/Test Suites/TR-API/Channels/ValidateChannelSample.ts
+++ TR API/Test Suites/TR-API/Channels/ValidateChannelSample.ts
@@ -1,7 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteEntity>
-   <description></description>
-   <name>Causes</name>
+   <description>Validate a simple channel.&#xd;
+Not Data Driven.</description>
+   <name>ValidateChannelSample</name>
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
@@ -10,12 +11,12 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>true</rerunImmediately>
-   <testSuiteGuid>bd84c208-c9be-4094-a531-fcbcb5f31132</testSuiteGuid>
+   <testSuiteGuid>e2dd1735-aa30-4fbf-8d97-bd536e31f7c2</testSuiteGuid>
    <testCaseLink>
-      <guid>9c2c179d-2c43-4117-a735-9534f68f53f6</guid>
+      <guid>3f147b07-5c0c-47c9-850a-b1440dde231f</guid>
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
-      <testCaseId>Test Cases/causes/ValidateCauses</testCaseId>
+      <testCaseId>Test Cases/channels/ValidateChannelSample</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
 </TestSuiteEntity>
---
+++ TR API/Test Suites/TR-API/NOMINAL.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Nominal test cases.</description>
+   <name>NOMINAL</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>bb890840-e852-4636-9451-7ada59e26794</testSuiteGuid>
+   <testCaseLink>
+      <guid>758fdbe3-c67e-4ee5-b247-1f932e653851</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/causes/ValidateCauseNoCategory</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5677602e-aca3-4ac3-a89e-dc95b9e5f5a4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/channels/ValidateChannelSample</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
